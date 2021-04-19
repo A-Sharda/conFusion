4f4d8f2ea3f2e5ebb9c2cc3470c83bfe7d07967c
Sharda Akella <sharda2209@gmai.com>
2021-04-19 12:47:42 +0530

    about and leader service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -53,6 +54,7 @@ import { ContactComponent } from './contact/contact.component';
     AppRoutingModule
   ],
   providers: [DishService,
+    LeaderService,
   PromotionService
    ],
   bootstrap: [AppComponent]
